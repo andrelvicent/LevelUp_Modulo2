9c3deef4564d136e72b813ad8edca1072908af5b
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-02 15:19:05 -0300

    Cadastrar mais de um usuário e sair do programa

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -9,18 +9,36 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         Map <String, String> alunos = new HashMap<String, String>();
-
+        boolean chave = true;
         System.out.println("---- Seja bem vindo ao André Classroom ----");
+        int opcao = 0;
 
-        System.out.println("Digite o nome Completo do aluno: ");
-        String nome = leitor.nextLine();
-        System.out.println("Digite o seu telefone: ");
-        String telefone = leitor.nextLine();
-        System.out.println("Digite o seu e-mail: ");
-        String email = leitor.nextLine();
+        while (chave == true){
+            // Menu - Apresentar uma série de opcoes pro usuario
+            System.out.println("Digite 1: Para cadastrar um aluno");
+            System.out.println("Digite 2: Para sair do programa");
+            opcao = leitor.nextInt();
+            leitor.nextLine();
 
-        alunos.put(email, "Nome: " + nome + " Telefone: " + telefone);
+            if (opcao == 1){
+                // Cadastrar um aluno
+                System.out.println("Digite o nome Completo do aluno: ");
+                String nome = leitor.nextLine();
+                System.out.println("Digite o seu telefone: ");
+                String telefone = leitor.nextLine();
+                System.out.println("Digite o seu e-mail: ");
+                String email = leitor.nextLine();
 
-        System.out.println(alunos);
+                // Fazendo o cadastro
+                alunos.put(email, "Nome: " + nome + " Telefone: " + telefone);
+            } else if (opcao == 2){
+                // Sair do menu
+                chave = false;
+            }else{
+                System.out.println("Por favor, digite um valor válido. ");
+            }
+        }
+
+
     }
 }
