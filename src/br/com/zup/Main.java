d50f16681de564a09e506b3a6a4d806a9a9edba4
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-02 14:51:08 -0300

    Criando o projeto

---
+++ src/br/com/zup/Main.java
@@ -0,0 +1,12 @@
+package br.com.zup;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
