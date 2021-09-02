5ebafcee4ac1521699a8ea4306ffe18c00082cdb
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-02 15:32:36 -0300

    Exibir lista de usuários

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -8,19 +8,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        Map <String, String> alunos = new HashMap<String, String>();
+        Map<String, String> alunos = new HashMap<String, String>();
         boolean chave = true;
         System.out.println("---- Seja bem vindo ao André Classroom ----");
         int opcao = 0;
 
-        while (chave == true){
+        while (chave == true) {
+
             // Menu - Apresentar uma série de opcoes pro usuario
             System.out.println("Digite 1: Para cadastrar um aluno");
-            System.out.println("Digite 2: Para sair do programa");
+            System.out.println("Digite 2: Para exibir a lista");
+            System.out.println("Digite 3: Para sair do programa");
+
             opcao = leitor.nextInt();
             leitor.nextLine();
 
-            if (opcao == 1){
+            if (opcao == 1) {
                 // Cadastrar um aluno
                 System.out.println("Digite o nome Completo do aluno: ");
                 String nome = leitor.nextLine();
@@ -31,10 +34,16 @@ public class Main {
 
                 // Fazendo o cadastro
                 alunos.put(email, "Nome: " + nome + " Telefone: " + telefone);
-            } else if (opcao == 2){
+            } else if (opcao == 2) {
+
+                for (String chaveAluno : alunos.keySet()) {
+                    System.out.println("Contato: " + alunos.get(chaveAluno) + " Email: " + chaveAluno);
+                }
+
+            } else if (opcao == 3) {
                 // Sair do menu
                 chave = false;
-            }else{
+            } else {
                 System.out.println("Por favor, digite um valor válido. ");
             }
         }
