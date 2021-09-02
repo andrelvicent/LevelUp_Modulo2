b06040f86249f1e27f7d5c98e4e0bdbdab32e3e9
André Luiz Vicente <andre.vicente@zup.com.br>
2021-09-02 14:58:29 -0300

    Cadastrar pelo menos um usuário

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -7,6 +7,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Map <String, String> alunos = new HashMap<String, String>();
 
+        System.out.println("---- Seja bem vindo ao André Classroom ----");
+
+        System.out.println("Digite o nome Completo do aluno: ");
+        String nome = leitor.nextLine();
+        System.out.println("Digite o seu telefone: ");
+        String telefone = leitor.nextLine();
+        System.out.println("Digite o seu e-mail: ");
+        String email = leitor.nextLine();
+
+        alunos.put(email, "Nome: " + nome + " Telefone: " + telefone);
+
+        System.out.println(alunos);
     }
 }
